1334a4e1eab97ac75d5c6fc0db81cf56f49a6827
Ritwik Baranwal <45766294+Vortez2471@users.noreply.github.com>
2019-11-29 10:03:31 +0530

    Add files via upload

---
+++ Functions.m
@@ -0,0 +1,16 @@
+%use wordpad to open functions documentation
+
+d=squareThisNumber(5)    %it return y only if the function is present in same directory
+
+% addpath('address')   %it helps in adding the directory into current program
+
+[a,b]=squareAndCubeThisNumber(5)
+
+X=[1 1;1 2;1 3]
+y=[1;2;3]
+theta=[0;1]
+j=costFunctionJ(X,y,theta)
+
+theta=[0;0]
+j=costFunctionJ(X,y,theta)
+
